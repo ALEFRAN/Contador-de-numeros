fdf920f0488644f8804de23ca02c6397b8b6a55b
ALEFRAN <alefran19@hotmail.com>
2024-03-25 19:12:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let show = document.querySelector('.show')
+
+let increment = 0
+
+document.addEventListener('click', e => {
+    //aumentar y decrementar el valor
+
+    if (e.target.matches('.increase')) {
+        increment++
+        show.innerHTML = increment
+    }else if (e.target.matches('.decrease')) {
+        increment--
+        show.innerHTML = increment
+    } else if (e.target.matches('.reset')) {
+        increment = 0
+        show.innerHTML = increment
+    }
+
+    //Cambiar color
+
+    if (increment < 0) {
+    show.style.color = 'red'
+    }else{
+    show.style.color = 'green'
+    }
+
+    if (increment == 0) show.style.color = "#222" 
+
+    })
+
+
+
